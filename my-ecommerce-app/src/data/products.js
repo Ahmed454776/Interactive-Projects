5fb3b9fbb41ae9696f9a99fcc106f56ec34692da
Syed Alauddin Ahmed Rizve <syedalauddin452@gmail.com>
2025-10-21 14:12:06 +0600

    added new images

--- my-ecommerce-app/src/data/products.js
+++ my-ecommerce-app/src/data/products.js
@@ -4,29 +4,29 @@ const products = [
     title: "Wireless Headphones",
     description: "High-quality wireless headphones with noise cancellation.",
     price: 120,
-    image: "https://www.shutterstock.com/image-photo/headphones-isolate-on-white-wireless-260nw-2466522991.jpg"
+    image: "../assets/wireless_headphone.jpg",
   },
   {
     id: 2,
     title: "Gaming Mouse",
     description: "Ergonomic gaming mouse with customizable buttons.",
     price: 45,
-    image: "https://static.vecteezy.com/system/resources/thumbnails/024/746/812/small/futuristic-computer-equipment-illuminates-dark-office-for-high-tech-competition-performance-generated-by-ai-free-photo.jpg"
+    image: "../assets/gaming_mouse.jpg",
   },
   {
     id: 3,
     title: "Mechanical Keyboard",
     description: "RGB mechanical keyboard with tactile switches.",
     price: 80,
-    image: "https://www.shutterstock.com/image-photo/gaming-keyboard-rgb-light-white-260nw-1920886418.jpg"
+    image: "../assets/mechanical_keyboard.jpg",
   },
   {
     id: 4,
     title: "Smart Watch",
     description: "Water-resistant smart watch with health tracking.",
     price: 150,
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnwiSK4Z83nnY8ippTM1AMeyJiRpSAeaZgckAGFVKuStv1dp-kcIHcfjCJ7lzIGMzj13c&usqp=CAU"
-  }
+    image: "../assets/smart_watch.jpg",
+  },
 ];
 
 export default products;
